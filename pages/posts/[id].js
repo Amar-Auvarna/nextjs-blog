5b1d9f1c3025f00f20d796caac0a26108a08a7e7
amar-auvarna <amar.singh@auvarna.com>
2020-10-13 11:00:31 +0530

    learn nextjs

---
+++ pages/posts/[id].js
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+// import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+// export default function Post({ postData }) {
+//   return (
+//     <Layout>
+//       <Head>{postData.title}</Head>
+//       <br />
+//       {postData.id}
+//       <br />
+//       <Date dateString={postData.date} />
+//       {/* {postData.date} */}
+//       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//     </Layout>
+//   )
+// }
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
